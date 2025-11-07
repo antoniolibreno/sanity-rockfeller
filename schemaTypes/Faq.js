b0faf32dd587f9d59aa4129a5352226d17975f8d
pablo <pablobertoncelo@gmail.com>
2025-11-06 23:32:29 -0300

    Adicionando o faq no Sanity

--- schemaTypes/Faq.js
+++ schemaTypes/Faq.js
@@ -1,43 +1,47 @@
-    import { defineField, defineType } from 'sanity'
-    export const faq = defineType({
-    name: 'faq',
-    title: 'faq',
-    type: 'document',
-    fields: [
-        defineField({
-        name: 'pergunta1',
-        title: 'Pergunta 1',
-        type: 'string',
-        }), 
-        defineField({
-        name: 'resposta1',
-        title: 'resposta 1',
-        type: 'string',
-        }),
+import { defineField, defineType } from 'sanity'
+export const faq = defineType({
+  name: 'faq',
+  title: 'faq',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'pergunta1',
+      title: 'Pergunta 1',
+      type: 'string',
+    }), 
+    defineField({
+      name: 'resposta1',
+      title: 'resposta 1',
+      type: 'text',
+    }),
 
 
-        defineField({
-        name: 'pergunta2',
-        title: 'Pergunta 2',
-        type: 'string',
-        }),
-        
-        defineField({
-        name: 'resposta2',
-        title: 'resposta 2',
-        type: 'string',
-        }),
+    defineField({
+      name: 'pergunta2',
+      title: 'Pergunta 2',
+      type: 'string',
+    }),
+    
+    defineField({
+      name: 'resposta2',
+      title: 'resposta 2',
+      type: 'text',
+    }),
 
-        defineField({
-        name: 'pergunta3',
-        title: 'pergunta3',
-        type: 'string',
-        }),
+     defineField({
+      name: 'pergunta3',
+      title: 'pergunta 3',
+      type: 'string',
+    }),
 
-        defineField({
-        name: 'resposta3',
-        title: 'resposta 3',
-        type: 'string',
-        })
-    ],
-    })
+     defineField({
+      name: 'resposta3',
+      title: 'resposta 3',
+      type: 'text',
+    }),
+
+
+
+    
+  ],
+})
--- schemaTypes/metodologiaType.js
+++ schemaTypes/metodologiaType.js
@@ -2,7 +2,7 @@ import { defineField, defineType } from 'sanity'
 
 export const metodologiaType = defineType({
   name: 'metodologia',
-  title: 'Metedologia',
+  title: 'Metodologia',
   type: 'document',
   fields: [
     defineField({
@@ -12,12 +12,12 @@ export const metodologiaType = defineType({
     }),
     defineField({
       name: 'Textometodologia1',
-      title: 'Texto da metodologia',
+      title: 'Texto da metodologia 1',
       type: 'string',
     }),
      defineField({
       name: 'Textometodologia2',
-      title: 'Texto da metodologia',
+      title: 'Texto da metodologia 2',
       type: 'string',
     }),
   ],
