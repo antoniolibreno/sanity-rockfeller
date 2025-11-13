bfe76fab3d907665dbd584470a756a7b4b614251
breno.antonioli@outlook.com <breno.antonioli@outlook.com>
2025-11-12 21:27:30 -0300

    editando metodologia.type

--- schemaTypes/metodologiaType.js
+++ schemaTypes/metodologiaType.js
@@ -10,15 +10,5 @@ export const metodologiaType = defineType({
       title: 'Imagem 1',
       type: 'image',
     }),
-    defineField({
-      name: 'Textometodologia1',
-      title: 'Texto da metodologia 1',
-      type: 'string',
-    }),
-     defineField({
-      name: 'Textometodologia2',
-      title: 'Texto da metodologia 2',
-      type: 'string',
-    }),
   ],
 })
